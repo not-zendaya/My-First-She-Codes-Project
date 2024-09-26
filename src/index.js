f05a4edc42350ce1290c5ecb12bc505ecefdb820
not-zendaya <ukzendaya@gmail.com>
2024-09-26 12:19:07 +0300

    added search engine

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector(".search-form");
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 function formatDate(date) {
